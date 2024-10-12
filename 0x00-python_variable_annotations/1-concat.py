a2be7ae31e57d3ed203544d14ba7ae37aa805cbc
Sesh-Gallagher <t.ntwana@gmail.com>
2024-10-11 20:40:30 -0700

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Module that write a type-annotated function concat
+that takes a string str1 and a string str2 as arguments
+and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Represents the type annotated function
+    Concat
+    """
+    return str1 + str2
